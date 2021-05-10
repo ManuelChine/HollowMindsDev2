960dcb42faf1a1bda3171764ea79ba27d79f9881
Umbertobf17 <umbertobf17@gmail.com>
2021-05-10 16:43:11 +0200

    Validation 2
    Aggiunte validazioni (da completare)

--- src/HollowMindsDev.BackEnd.ApplicationCore/Entities/Silos/Silo.cs
+++ src/HollowMindsDev.BackEnd.ApplicationCore/Entities/Silos/Silo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,27 @@ namespace HollowMindsDev.BackEnd.ApplicationCore.Entities.Silos
 {
     public class Silo : Entity<int>
     {
+        [Required]
         public decimal Height { get; set; }
+
+        [Required]
         public decimal Diameter { get; set; }
+
+        [Required]
         public decimal Capacity { get; set; }
+
+        [Required]
         public int YearProd { get; set; }
+
+        [Required]
         public string Location { get; set; }
 
         //FK
+
+        [Required]
         public int IdBlock { get; set; }
+
+        [Required]
         public int IdLimit { get; set; }
     }
 }
