17c5e619441cb83d850fff30ec279e542989cbbe
Umbertobf17 <umbertobf17@gmail.com>
2021-05-10 15:30:38 +0200

    Aggiunta Validazione

--- src/HollowMindsDev.BackEnd.ApplicationCore/Entities/Silos/Block.cs
+++ src/HollowMindsDev.BackEnd.ApplicationCore/Entities/Silos/Block.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace HollowMindsDev.BackEnd.ApplicationCore.Entities.Silos
 {
     public class Block : Entity<int>
     {
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
     }
 }
--- src/HollowMindsDev.BackEnd.ApplicationCore/Entities/Silos/Limit.cs
+++ src/HollowMindsDev.BackEnd.ApplicationCore/Entities/Silos/Limit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,29 @@ namespace HollowMindsDev.BackEnd.ApplicationCore.Entities.Silos
 {
     public class Limit : Entity<int>
     {
+        [Range(-30, 100,
+            ErrorMessage = "Value For Temperature must be between {1}°C and {2}°C.")]
+        [Required]
         public decimal Temperature { get; set; }
+
+        [Range(0.5, 10 ,
+            ErrorMessage = "Value For Pressure must be between {1} and {2}.")]
+        [Required]
         public decimal Preassure { get; set; }
+
+        [Required]
         public int LevelMax { get; set; }
+
+        [Required]
         public int LevelMin { get; set; }
+
+        [Range(0, 100,
+            ErrorMessage = "Value For Umidity must be between {1}% and {2}%.")]
+        [Required]
         public decimal Umidity { get; set; }
+
+        [StringLength(30)]
+        [Required]
         public string Material { get; set; }
     }
 }
