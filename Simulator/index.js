441962c261df0ba22ca9dc7240886075609810b7
Manuel-Cestaro <83578640+Manuel-Cestaro@users.noreply.github.com>
2021-05-14 12:48:55 +0200

    quasi funzionano 2 funz

--- Simulator/index.js
+++ Simulator/index.js
@@ -1,13 +1,18 @@
 const got = require('got');
+const axios = require('axios')
 
 const time1 = 60000; // 1 min
-const time2 = 5000;
-const time3 = 5000;
+const time2 = 300000; // 5 min
+const time3 = 900000; // 15 min
 
 //generatore di numeri decimeli casuali compresi tra parametri
 function getRandom(min, max) {
     num = Math.random() * (max - min) + min; //Il max è escluso e il min è incluso
     return Math.floor(num*100)/100;
+  }
+
+  function getRandomBool() {
+    return Math.floor(Math.random() * 2);
   }
 
   //trovare la data corrente in formato letto da C#
@@ -34,6 +39,7 @@ function getRandom(min, max) {
 //creare lista di date in cui salvare le ultime ore in cui solo avvenuti dei decrementi
 const lastDecrement = [];
 const n = 21600000; //6 ore
+var checkIncr =  true;
 
 const link = 'http://localhost:42079/api/';
 
@@ -43,7 +49,6 @@ async function decrement(time, link) {
         var list = JSON.parse(res.body);
         //console.log(list[1]); for debug
         for (var i=0; i<list.length; i++){
-        //list.forEach(element => {
             
             //creare lista di bool (sensori)
             const sensors = [list[i].measurement.sensor0, list[i].measurement.sensor1, list[i].measurement.sensor2, 
@@ -88,18 +93,27 @@ async function decrement(time, link) {
                 dropCheck: 0,//non implementato, il sensore reale non è in grado di gestirlo. Lascio il valore di dafault
                 idSilo: list[i].measurement.idSilo
             };
+            // console.log(result); only for debug
 
-            console.log(result);
-
-            //reply.view(link + 'api', {result})//da rifare e testare (per test -> stampo a console)
             //uso API di inserimento measurement
+            axios
+                .post(link + 'Measurement', JSON.parse(JSON.stringify(result)))
+                .then((res) => {
+                    console.log(`statusCode: ${res.statusCode}`)
+                    console.log(res)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
 
-            //se il contatore dei livelli è a 0 far partire l'incremento 
-            //if(!sensors[0]){
-                //richiamre funzione incremento
+            //se il contatore dei livelli è a 0 far partire l'incremento
+            if(sensors[0]===0 && checkIncr){
                 //settare un variabile per fare in modo che la funzione non venga fatta partire più volte, 
                 //causa: la funzione è asincrono e ci mette 5 min ma il controllo è ripetuto ogni min
-            //}
+                checkIncr = false;
+                //richiamre funzione incremento
+                
+            }
         };
    // }, time);
 }
@@ -109,9 +123,53 @@ async function decrement(time, link) {
 (async function(){
     try{
         decrement(time1, link);
+        //errorGenerator(time3, link);
     } catch(e) {
         console.log('errore');
     }
     
 })();
 //l'attesa DEVE essere ASINCRONA (da controllare)
+
+async function errorGenerator(time, link) {
+    //setTimeout(async function(link) {
+        const res = await got(link + 'ViewModel');
+        var list = JSON.parse(res.body);
+        //console.log(list[1]); for debug
+        for (var i=0; i<list.length; i++){
+        
+            //composizione dell'oggetto: dati casuali NON compresi tra i limiti
+            const result = {
+                sensor0: getRandomBool(),
+                sensor1: getRandomBool(),
+                sensor2: getRandomBool(),
+                sensor3: getRandomBool(),
+                sensor4: getRandomBool(),
+                sensor5: getRandomBool(),
+                sensor6: getRandomBool(),
+                sensor7: getRandomBool(),
+                pressure: getRandom(list[i].limit.pressure*0.85, list[i].limit.pressure*1.15),
+                temperatureTop: getRandom(list[i].limit.temperature*0.85, list[i].limit.temperature*1.15),
+                temperatureBottom: getRandom(list[i].limit.temperature*0.85, list[i].limit.temperature*1.15),
+                umidityTop: getRandom(list[i].limit.umidity*0.85, list[i].limit.umidity*1.15),
+                umidityBottom: getRandom(list[i].limit.umidity*0.85, list[i].limit.umidity*1.15),
+                time: currentDateTime(),
+                dropCheck: 0,//non implementato, il sensore reale non è in grado di gestirlo. Lascio il valore di dafault
+                idSilo: list[i].measurement.idSilo
+            };
+
+            // console.log(result); only for debug
+
+            //uso API di inserimento measurement
+            axios
+                .post(link + 'Measurement', JSON.parse(JSON.stringify(result)))
+                .then((res) => {
+                    console.log(`statusCode: ${res.statusCode}`)
+                    console.log(res)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+        };
+   // }, time);
+}
