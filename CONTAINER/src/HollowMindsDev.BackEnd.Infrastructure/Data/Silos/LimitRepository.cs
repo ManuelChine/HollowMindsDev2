ad77873a5c472a00bc734376be57cbee1711af40
Manuel Chinellato <manuelchine@libero.it>
2021-05-18 10:09:11 +0200

    container

---
+++ CONTAINER/src/HollowMindsDev.BackEnd.Infrastructure/Data/Silos/LimitRepository.cs
@@ -0,0 +1,106 @@
+﻿using Dapper;
+using HollowMindsDev.BackEnd.ApplicationCore.Entities.Silos;
+using HollowMindsDev.BackEnd.ApplicationCore.Interfaces.ISilos;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HollowMindsDev.BackEnd.Infrastructure.Data.Silos
+{
+    public class LimitRepository : ILimitRepository
+    {
+        private readonly string _connectionString;
+
+        public LimitRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Database");
+        }
+
+        public IEnumerable<Limit> GetAll()
+        {
+            const string query = @"
+SELECT
+    temperature as Temperature,
+    pressure as Pressure,
+    level_max as LevelMax,
+    level_min as LevelMin,
+    umidity as Umidity,
+    material as Material
+FROM limit_silo;";
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.Query<Limit>(query);
+        }
+
+        public Limit GetById(int id)
+        {
+            const string query = @"
+SELECT
+    temperature as Temperature,
+    pressure as Pressure,
+    level_max as LevelMax,
+    level_min as LevelMin,
+    umidity as Umidity,
+    material as Material
+FROM limit_silo
+WHERE idLimit = @idL";
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.QueryFirstOrDefault<Limit>(query, new { idL = id });
+        }
+
+        public void Delete(int id)
+        {
+            const string query = @"
+DELETE FROM limit_silo
+WHERE idLimit = @idL;";
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Execute(query, new { idL = id });
+        }
+
+        public void Insert(Limit model)
+        {
+            const string query = @"
+INSERT INTO limit_silo (temperature, umidity, pressure, level_max, level_min, material)
+VALUES (@Temperature, @Umidity, @Pressure, @LevelMax, @LevelMin, @Material);";
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Execute(query, model);
+        }
+
+        public Limit LimitBySilo(int idSilo)
+        {
+            const string query = @"
+SELECT
+    limit_silo.idLimit as Id,
+    temperature as Temperature,
+    pressure as Pressure,
+    level_max as LevelMax,
+    level_min as LevelMin,
+    umidity as Umidity,
+    material as Material
+FROM limit_silo
+INNER JOIN silo
+ON limit_silo.idLimit = silo.idLimit
+WHERE silo.idSilo = @id";
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.QueryFirstOrDefault<Limit>(query, new { id = idSilo });
+        }
+
+        public void Update(Limit model)
+        {
+            const string query = @"
+UPDATE limit_silo
+SET temperature = @Temperature,
+    pressure = @Pressure,
+    level_max = @LevelMax,
+    level_min = @LevelMin,
+    umidity =  @Umidity,
+    material = @Material
+WHERE idLimit = @Id;";
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Execute(query, model);
+        }
+    }
+}
---
+++ CONTAINER/src/HollowMindsDev.BackEnd.Infrastructure/Data/Users/AdminRepository.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using HollowMindsDev.BackEnd.ApplicationCore.Entities.Users;
+using HollowMindsDev.BackEnd.ApplicationCore.Interfaces.IUsers;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HollowMindsDev.BackEnd.Infrastructure.Data.Users
+{
+    public class AdminRepository : IAdminRepository
+    {
+        private readonly string _connectionString;
+
+        public AdminRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Database");
+        }
+        public void Delete(int id)
+        {
+            const string query = @"
+DELETE FROM administrator
+WHERE idAdmin = @idA;";
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Execute(query, new { idA = id });
+        }
+
+        public IEnumerable<Admin> GetAll()
+        {
+            const string query = @"
+SELECT
+    mail as EMail
+FROM administrator;";
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.Query<Admin>(query);
+        }
+
+        public Admin GetById(int id)
+        {
+            const string query = @"
+SELECT
+    mail as EMail
+FROM administrator
+WHERE idAdmin = @idA;";
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.QueryFirstOrDefault<Admin>(query);
+        }
+
+        public bool IfIsAdmin(string mail) //Opzionale
+        {
+            //int x = 2;
+            return true;
+        }
+
+        public void Insert(Admin model)
+        {
+            const string query = @"
+INSERT INTO administrator (mail)
+VALUES (@email);";
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Execute(query, model);
+        }
+
+        public void Update(Admin model)
+        {
+            const string query = @"
+UPDATE administrator
+SET mail = @email
+WHERE idAdmin = @Id;";
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Execute(query, model);
+        }
+    }
+}
